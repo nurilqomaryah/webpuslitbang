d6435da23238a18fe0136c722568cae7a94650f5
Ibnu Aulia Nugraha Alihaq <ibnuauliana@gmail.com>
2021-08-30 13:42:33 +0700

    Commit for change layout

--- resources/views/layoutcrud.blade.php
+++ resources/views/layoutcrud.blade.php
@@ -1,25 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manajemen Data Website</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/templatecrud/sb-admin-2.min.css') }}"/>
-{{--    <link rel="stylesheet" type="text/css" href="{{ asset('css/templatecrud/bootstrap.css') }}"/>--}}
-{{--    <link rel="stylesheet" type="text/css" href="{{ asset('css/templatecrud/font-awesome.css') }}"/>--}}
-{{--    <link rel="stylesheet" type="text/css" href="{{ asset('css/templatecrud/custom-styles.css') }}"/>--}}
-{{--    <link rel="stylesheet" type="text/css" href="{{ asset('css/templatecrud/font-opensans.css') }}"/>--}}
-{{--    <link rel="stylesheet" type="text/css" href="{{ asset('js/templatecrud/dataTables/dataTables.bootstrap.css')}}"/>--}}
-{{--    <link rel="stylesheet" type="text/css" href="{{ asset('js/templatecrud/morris/morris-0.4.3.min.css')}}"/>--}}
-</head>
-<body id="page-top">
-<div id="wrapper">
-    @include('sidebar')
-    @yield('main')
-</div>
+<html>
+    <head>
+        <title>INV-PN | @yield('pageTitle')</title>
+        <meta name="description" content="Aplikasi Inventarisasi Piutang Negara">
+        <meta name="keywords" content="DJKN, PN, Piutang, Piutang Negara">
+        <meta name="author" content="DJKN | Piutang Negara dan Kekayaan Negara Lainnya | @lastaegis">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/templatecrud/sb-admin-2.min.css') }}"/>
+    </head>
+    <body id="page-top">
+    <div id="wrapper">
+        @include('sidebar')
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                @include('menu.top-menu')
+                <div class="container-fluid">
+                    @yield('main')
+                </div>
+            </div>
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright © BPKP | PUSLITBANGWAS NC</span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    </body>
+</html>
 <script type="text/js" src="{{ asset('js/app.js')}}" ></script>
 <script type="text/js" src="{{ asset('js/templatecrud/sb-admin-2.min.js') }}" ></script>
 <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
 <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-</body>
-</html>
+
